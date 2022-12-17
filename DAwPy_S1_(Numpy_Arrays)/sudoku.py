855d7fcd315d5a73b41145be08fa6ed62b4ddde6
4dsolutions <kirby.urner@gmail.com>
2022-12-17 12:58:09 -0800

    update sudoku

--- DAwPy_S1_(Numpy_Arrays)/sudoku.py
+++ DAwPy_S1_(Numpy_Arrays)/sudoku.py
@@ -1,89 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 10:10:02 2022
+
+@author: mac
+"""
+
 import numpy as np
-import math
+from random import shuffle
+import time
 
-N = 3
+zero_patch = np.zeros((3,3), dtype = int)
 
-# rewrite of https://www.tutorialspoint.com/valid-sudoku-in-python
-def isValidSudoku(M): 
-    '''
-    Check a sudoku matrix:
-        A 9x9 sudoku matrix is valid iff every:
-          row contains 1 - 9 and
-          col contains 1 - 9 and
-          3x3 contains 1 - 9
-        0 is used for blank entry
-    '''
-    for i in range(9):
-        row = {}
-        col = {}
-        block = {}
-        row_cube = N * (i//N)
-        col_cube = N * (i%N)
-        for j in range(N*N):
-            if M[i][j] != 0 and M[i][j] in row:
-                return False
-            row[M[i][j]] = 1
-            if M[j][i] != 0 and M[j][i] in col:
-                return False
-            col[M[j][i]] = 1
-            rc = row_cube + j//N
-            cc = col_cube + j%N
-            if M[rc][cc] in block and M[rc][cc] != 0:
-                return False
-            block[M[rc][cc]]=1
+def init_board():
+    
+    def diag_patch():
+        one_nine = list(range(1, 10))
+        shuffle(one_nine)
+        return np.array(one_nine, dtype=int).reshape(3,3)
+    
+    row0       = np.concatenate([diag_patch(), 
+                                 zero_patch, 
+                                 zero_patch], axis = 1)
+    
+    row1       = np.concatenate([zero_patch, 
+                                 diag_patch(), 
+                                 zero_patch], axis = 1)
+    
+    row2       = np.concatenate([zero_patch, 
+                                 zero_patch, 
+                                 diag_patch()], axis = 1)
+    
+    board      = np.concatenate([row0, row1, row2], axis=0)
+    
+    return board
+
+def find_empty(board):
+    rtnval = False
+    results = np.where(board == 0)
+    if results[0].size > 0:
+        rtnval = [results[0][0], results[1][0]]
+    return rtnval
+
+def check(board, value, pos):
+    
+    def in_patch(r, c):
+        if 0 <= row <= 2:
+            patch_row = 0
+        if 3 <= row <= 5:
+            patch_row = 3
+        if 6 <= row <= 9:
+            patch_row = 6   
+        if 0 <= col <= 2:
+            patch_col = 0
+        if 3 <= col <= 5:
+            patch_col = 3
+        if 6 <= col <= 9:
+            patch_col = 6    
+        
+        return value in board[patch_row:patch_row+3,
+                              patch_col:patch_col+3]
+            
+    row, col = pos
+    if value in board[row] or value in board[:,col]:
+        return False
+    if in_patch(row, col):
+        return False
     return True
-    
-def generate_sudoku_puzzles(run_size, seed):  
-    
-    order = int(math.sqrt(run_size))
-    count = 0
-    valid = 0
-    empty = []
-    np.random.seed(seed) # for reproducible results
-    
-    for k in range(order):
-        for l in range(order):
+        
+def recurse(board):
+    position = find_empty(board)
+    if position:
+        for candidate in range(1, 10):
+            if check(board, candidate, position):
+                board[position[0], position[1]] = candidate
+                # print(board)
+                return recurse(board)
+        # print("Unsolved")
+        return False
+    else:
+        return True
 
-            A = np.fromfunction(lambda i, j: ((k*i + l+j) % (N*N)) + 1, (N*N, N*N), dtype=int)
-            B = np.random.randint(2, size=(N*N, N*N))
-            empty.append(np.count_nonzero(B))
-            C = A*B
-            count += 1
+def generate():
+    attempt = 0
+    start_time = time.time()
+    while True:
+        # print(attempt)
+        B = init_board()
+        if not recurse(B):  # updates B until no more empty
+            attempt += 1
+            continue        # dead end, try new B
+        break  # solution found
+    stop_time = time.time()
+    the_time = stop_time - start_time
+    print(f"Solved; Time: {the_time}")
+    return B
 
-            if isValidSudoku(C):
-                valid += 1
-                last = C
-                print('C(',k,l,') is valid sudoku:')
-                print(C) # Uncomment for puzzle
-
-    print('Tried', count, 'valid', valid, 'yield', round(valid/count, 3)*100, '%', 'Average Clues', round(sum(empty)/len(empty)))
-    return(last)
-
-posTest = np.array([(0, 7, 0, 0, 4, 0, 0, 6, 0), \
-                    (3, 0, 0, 5, 0, 7, 0, 0, 2), \
-                    (0, 0, 5, 0, 0, 0, 3, 0, 0), \
-                    (0, 4, 0, 3, 0, 6, 0, 5, 0), \
-                    (6, 0, 0, 0, 0, 0, 0, 0, 8), \
-                    (0, 1, 0, 2, 0, 8, 0, 3, 0), \
-                    (0, 0, 7, 0, 0, 0, 4, 0, 0), \
-                    (1, 0, 0, 8, 0, 2, 0, 0, 9), \
-                    (0, 6, 0, 0, 9, 0, 0, 1, 0), \
-                    ])
-
-negTest = np.array([(0, 7, 0, 0, 4, 0, 0, 6, 2), \
-                    (3, 0, 0, 5, 0, 7, 0, 0, 2), \
-                    (0, 0, 5, 0, 0, 0, 3, 0, 0), \
-                    (0, 4, 0, 3, 0, 6, 0, 5, 0), \
-                    (6, 0, 0, 0, 0, 0, 0, 0, 8), \
-                    (0, 1, 0, 2, 0, 8, 0, 3, 0), \
-                    (0, 0, 7, 0, 0, 0, 4, 0, 0), \
-                    (1, 0, 0, 8, 0, 2, 0, 0, 9), \
-                    (0, 6, 0, 0, 9, 0, 0, 1, 0), \
-                    ])
-
-print('Positive Quality Control Test:', isValidSudoku(posTest))
-print('Negative Quality Control Test:', isValidSudoku(negTest))
-
-print(generate_sudoku_puzzles(10000, 0))
-
-
+def verify(board):
+    print(np.sum(board, axis=0))
+    print(np.sum(board, axis=1))
+    panel_sums = []
+    for vert_slice in np.vsplit(board, 3):
+        for panel in np.hsplit(vert_slice, 3):
+            panel_sums.append(np.sum(panel))
+    print(panel_sums)
